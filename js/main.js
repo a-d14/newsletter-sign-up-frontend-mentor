342f7c5658eeb86b13ca5e37d027df838c6c0e59
Aakash Dasgupta <dasgupta_aakash@ymail.com>
2025-02-22 19:41:50 -0600

    (feat) added form validation

--- js/main.js
+++ js/main.js
@@ -5,24 +5,40 @@ const btnSubscribe = document.querySelector(".btn-subscribe");
 const btnDismiss = document.querySelector(".btn-dismiss");
 
 const form = document.querySelector('form');
+const email = document.querySelector("form input[type='email']");
 
 const footer = document.querySelector("footer");
+
+function switchScreen() {
+    signupComponent.classList.toggle('hidden');
+    signupSuccessComponent.classList.toggle('hidden');
+    footer.classList.toggle('hidden');
+
+    email.value = '';
+}
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    // const formData = new FormData(e.target);
-    // const data = Object.fromEntries(formData);
-    // console.log(data);
+    const emailErrorMsg =  email.previousElementSibling.querySelector('.error-message');
 
-    
-
-    console.log(formInputs);
+    if(email.validity.valid) {
+        email.classList.remove('error');
+        emailErrorMsg.classList.add('hidden');
+        switchScreen();
+    } else {
+        emailErrorMsg.classList.remove('hidden');
+        email.classList.add('error');
+    }
     
 });
 
-[btnSubscribe, btnDismiss].forEach((btn) => btn.addEventListener('click', () => {
-    signupComponent.classList.toggle('hidden');
-    signupSuccessComponent.classList.toggle('hidden');
-    footer.classList.toggle('hidden');
-}));
+btnDismiss.addEventListener('click', () => {
+    switchScreen();
+});
+
+// [btnSubscribe, btnDismiss].forEach((btn) => btn.addEventListener('click', () => {
+//     signupComponent.classList.toggle('hidden');
+//     signupSuccessComponent.classList.toggle('hidden');
+//     footer.classList.toggle('hidden');
+// }));
